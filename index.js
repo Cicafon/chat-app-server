65866438b38996bc25d9f551978c930fa0915906
Krisztina Fekete <krisztina_fekete@epchzurw031f.home>
2022-06-21 16:21:45 +0200

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 
 const router = require("./router");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
