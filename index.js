c0915d40465a6c7cd8537aa755f93a9b6a2acccd
Krisztina Fekete <krisztina_fekete@epchzurw031f.home>
2022-06-21 16:06:24 +0200

    test

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const http = require("http");
+const cors = require('cors')
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
+const router = require("./router");
+
+const PORT = process.env.port || 5000;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(router);
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("a user connected!!!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  //user generated messages
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left!`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
---
+++ users.js
@@ -0,0 +1,33 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  if (existingUser) {
+      console.log("error: user is taken")
+    return { error: "Username is taken" };
+  }
+
+  const newUser = { id, name, room };
+  users.push(newUser);
+
+  return {user: newUser};
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
